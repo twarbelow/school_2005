397b9877f7294c17e4575f58439dab352fcc0df6
“Taija” <taija.warbelow@google.com>
2020-05-06 12:41:51 -0700

    Finish iteration 1 and 2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
+class School
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
 
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    end_time = start_time.to_i + hours_in_school_day
+    end_time = "#{end_time}:00"
+  end
+
+
+end
