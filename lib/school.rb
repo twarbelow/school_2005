9e597f6e5f309972871868336836a3dede77a6e1
“Taija” <taija.warbelow@google.com>
2020-05-06 13:08:44 -0700

    Complete Iteration 3

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,14 @@
 class School
   attr_reader :start_time,
               :hours_in_school_day,
-              :student_names
+              :student_names,
+              :standard_student_names
 
   def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
     @student_names = []
+    @standard_student_names = []
   end
 
   def add_student_name(name)
@@ -16,6 +18,17 @@ class School
   def end_time
     end_time = start_time.to_i + hours_in_school_day
     end_time = "#{end_time}:00"
+  end
+
+  def is_full_time?
+    @hours_in_school_day >= 4
+  end
+
+  def standard_student_names
+    @student_names.each do |name|
+      @standard_student_names << name.capitalize
+    end
+    @standard_student_names
   end
 
 
